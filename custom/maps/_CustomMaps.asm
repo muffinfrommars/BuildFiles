0f9af1e1e632e6acb83b63ce6650c4f1d67791cc
muffinfrommars <awesomedude307@gmail.com>
2022-09-29 11:54:33 -0700

    got damn osian bug fixed

--- custom/maps/_CustomMaps.asm
+++ custom/maps/_CustomMaps.asm
@@ -1,1 +1,9 @@
 ;custom maps
+
+* = $546950
+.logical lorom($546950, 1)
+
+aedcutscene
+.binary "aedcutscene.bin.fe4"
+
+.here
--- custom/tilesets/_CustomTilesets.asm
+++ custom/tilesets/_CustomTilesets.asm
@@ -1,5 +1,21 @@
 ;plains tileset related info
 
+* = $476743
+.logical lorom($476743, 1)
+
+ptiles
+.binary "plainsTiles.dat.fe4"
+
+.here
+
+* = $4A5190
+.logical lorom($4A5190, 1)
+
+ptileset
+.binary "plainsTileset.dat.fe4"
+
+.here
+
 ;castle tileset related info
 
 * = $3F8000 ; NOTE: overrides vanilla Village animations
