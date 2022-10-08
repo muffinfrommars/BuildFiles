7fc47f66cd1dfb4ecc66b426c94bed32b69c25d8
muffinfrommars <awesomedude307@gmail.com>
2022-10-08 15:07:40 -0700

    somewhat functioning first chapter, finally

--- custom/maps/_CustomMaps.asm
+++ custom/maps/_CustomMaps.asm
@@ -6,6 +6,10 @@
     aedcutscene    .binary "aedcutscene.bin.fe4"
     village        .binary "village.bin.fe4"
     overworld      .binary "overworld.bin.fe4"
+    mapone         .binary "mapone.bin.fe4"
+    maptwo         .binary "maptwo.bin.fe4"
+;   mapthree       .binary "mapthree.bin.fe4"
+;   mapfour        .binary "mapfour.bin.fe4"
 
 .here
 
